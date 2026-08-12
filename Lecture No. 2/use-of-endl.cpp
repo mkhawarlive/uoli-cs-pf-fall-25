c16dc9cf952102b4c3a0fcdb144a2eb83e7abec6
khawarnutkani <m_khawar@live.com>
2026-08-12 20:05:13 +0500

    12-August-2023 11:30:00 AM

--- Lecture No. 2/use-of-endl.cpp
+++
@@ -1,9 +0,0 @@
-# include <iostream>
-
-using namespace std;
-
-int main (){
-    cout << "My Name is Ashir" << endl;
-    cout << "I am student of BS CS 1st semester \n";
-    cout << "In University of Loralai";
-}
